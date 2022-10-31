7d8cc9e606d59199463cfc3e0f4c1486bd0055fb
shraddhabhuju <Sirtombombadil@protonmail.com>
2022-10-31 14:00:09 +0545

    feat(new): new feature
    new feature try out

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { setDefaultWasm } from "@certusone/wormhole-sdk";
-
 setDefaultWasm("node");
-
 export * from "./evm/zebecEthBridgeClient";
 export * from "./portalTransfer";
 export * from "./solana";
