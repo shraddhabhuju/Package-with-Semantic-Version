5913e71b34dda27528085ae99c177dd9ab5544bc
shraddhabhuju <Sirtombombadil@protonmail.com>
2022-10-31 14:03:29 +0545

    perf(new): perfomanace commit check
    check

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { setDefaultWasm } from "@certusone/wormhole-sdk";
+
 setDefaultWasm("node");
+
 export * from "./evm/zebecEthBridgeClient";
 export * from "./portalTransfer";
 export * from "./solana";
